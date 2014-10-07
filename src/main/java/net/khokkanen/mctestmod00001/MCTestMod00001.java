a7bd98bea732515054cc34acadf42e42770af6b6
Kim Hokkanen <kim.hokkanen@iki.fi>
2014-10-07 23:57:15 +0300

    Basic mod class

---
+++ src/main/java/net/khokkanen/mctestmod00001/MCTestMod00001.java
@@ -0,0 +1,28 @@
+package net.khokkanen.mctestmod00001;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "MCTestMod00001", name = "Minecraft Test Mod 00001", version = "1.7.10-0.1")
+public class MCTestMod00001
+{
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
